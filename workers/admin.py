6f9dfd89c2c23c2fd82069ae2408f465963fdb6a
Rob Berry <rob@luvhate.us>
2009-08-01 01:24:06 +0100

    hide some seldom used options in the admin

--- workers/admin.py
+++ workers/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 from models import Employee, Skill
@@ -6,13 +7,23 @@ from models import Employee, Skill
 class EmployeeAdmin(admin.StackedInline):
     model = Employee
     fk_name = 'user'
-    # max_num = 1
+    max_num = 1
     verbose_name = 'profile'
     verbose_name_plural = 'Profile'
     filter_horizontal = 'skill',
 
 class EmployeeUserAdmin(UserAdmin):
     inlines = [EmployeeAdmin]
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
+        (_('Groups'), {'fields': ('groups',), 'classes': ('collapse',)}),
+        (_('Permissions'), {
+            'fields': ('is_staff', 'is_active', 'is_superuser',
+                       'user_permissions'),
+            'classes': ('collapse',),
+        }),
+    )
 
 admin.site.unregister(User)
 admin.site.register(User, EmployeeUserAdmin)
