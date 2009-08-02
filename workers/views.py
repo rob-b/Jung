ea607368ce2f94d5c877165c39d464fd71af6eac
Rob Berry <rob@luvhate.us>
2009-08-02 23:20:06 +0100

    skill_detail view

--- workers/views.py
+++ workers/views.py
@@ -1,9 +1,10 @@
+from django.shortcuts import get_object_or_404
 from hostel.decorators import rendered
-from models import Employee
+from models import Employee, Skill
 
 @rendered
-def employee_list(request):
-    context = {
-        'object_list': Employee.objects.select_related(),
+def skill_detail(request, skill):
+    skill = get_object_or_404(Skill, title=skill)
+    return 'workers/skill_detail.html', {
+        'object': skill,
     }
-    return 'workers/employee_list.html', context
