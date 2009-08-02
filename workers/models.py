bcbe72801f14f8a9b989c6b6f2238c14f8741d33
Rob Berry <rob@luvhate.us>
2009-08-02 21:43:30 +0100

    post_save signal to create a profile for a new user

--- workers/models.py
+++ workers/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.db import IntegrityError
 from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from django.conf import settings
 from hostel.storage import AttachmentStorage
 import os.path
 
@@ -8,6 +11,21 @@ def rename_avatar(instance, filename):
     stem, ext = os.path.splitext(filename)
     filename = filename.replace(stem, username)
     return os.path.join('avatars', filename)
+
+def create_employee_profile(sender, **kwargs):
+    if 'created' not in kwargs or not kwargs['created']:
+        return
+    if not hasattr(settings, 'AUTH_PROFILE_MODULE'):
+        return
+    user = kwargs.get('instance')
+    app_label, model_name = settings.AUTH_PROFILE_MODULE.split('.')
+    model = models.get_model(app_label, model_name)
+    try:
+        profile = model.objects.create(user=user)
+    except Exception, e:
+        raise e
+models.signals.post_save.connect(create_employee_profile, sender=User)
+
 
 class Employee(models.Model):
 
@@ -19,7 +37,8 @@ class Employee(models.Model):
                                default='avatars/default.jpg',
                                upload_to=rename_avatar,
                                storage=AttachmentStorage(overwrite=True),)
-    role = models.ForeignKey('Role', verbose_name=_('Role'), unique=True)
+    role = models.ForeignKey('Role', verbose_name=_('Role'),
+                             blank=False, null=True)
 
     def __unicode__(self):
         return _('Employee profile for %s') % self.user.username
