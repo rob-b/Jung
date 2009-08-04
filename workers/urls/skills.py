70ff3c6c82efa53372eedd3eacbcdd633f6ed71b
Rob Berry <rob@luvhate.us>
2009-08-04 18:40:35 +0100

    add url for skill_list and link to it in base.html

--- workers/urls/skills.py
+++ workers/urls/skills.py
@@ -1,7 +1,8 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('workers.views',
-    url(r'^(?P<skill>[\s\w]+)/$',
+    url(r'^$', 'skill_list', name='workers_skill_list'),
+    url(r'^(?P<skill>[-\w]+)/$',
         'skill_detail',
         name='workers_skill_detail'),
 )
