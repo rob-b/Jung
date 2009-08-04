9d19b838d09d57f198f2d8e8cae2e9e7abee500d
Rob Berry <rob@luvhate.us>
2009-08-04 18:44:06 +0100

    skill_list view

--- workers/views.py
+++ workers/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import get_object_or_404
+from django.db.models import Count
 from hostel.decorators import rendered
 from models import Employee, Skill
 
@@ -7,4 +8,11 @@ def skill_detail(request, skill):
     skill = get_object_or_404(Skill, slug=skill)
     return 'workers/skill_detail.html', {
         'object': skill,
+    }
+
+@rendered
+def skill_list(request):
+    skills = Skill.objects.annotate(total_employees=Count('employee'))
+    return 'workers/skill_list.html', {
+        'object_list': skills,
     }
