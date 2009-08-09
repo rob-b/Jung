e9b37076d0773ec2291549e3815aa42f09bd9d4b
Rob Berry <rob@luvhate.us>
2009-08-09 22:01:41 +0100

    validate new users against profiles.urls

--- enrollment/forms.py
+++ enrollment/forms.py
@@ -3,14 +3,14 @@ from django.core.urlresolvers import resolve, Resolver404
 from django.utils.translation import ugettext_lazy as _
 from registration.forms import RegistrationFormUniqueEmail
 from urlparse import urlparse
+from profiles import urls
 
 class EnrollmentForm(RegistrationFormUniqueEmail):
 
     def clean_username(self):
         username = super(EnrollmentForm, self).clean_username()
-        try:
-            resolve(urlparse('/' + username + '/')[2])
-        except Resolver404:
-            return username
-        raise forms.ValidationError(_('This username is already taken. Please'
-                                      ' choose another'))
+        for pattern in urls.urlpatterns:
+            if pattern.resolve(username+'/'):
+                msg = _('This username is already taken. Please choose another')
+                raise forms.ValidationError(msg)
+        return username
