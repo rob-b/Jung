5ae4a3b4239febd1b9ea64ee8dd445a3ddd0854d
Rob Berry <rob@luvhate.us>
2009-08-14 11:52:08 +0100

    create the whole calendar in the inclusion tag
    this gives more control over the layout than extending the builtin HTMLCalendar

--- schedule/templatetags/schedule_tags.py
+++ schedule/templatetags/schedule_tags.py
@@ -1,13 +1,75 @@
 from django import template
 from django.utils.safestring import mark_safe
+from datetime import date
+from calendar import Calendar
+from calendar import day_abbr
 from schedule.utils import TaskCalendar
 register = template.Library()
+
+
+class Day(object):
+
+    def __init__(self, day, weekday, month, year):
+        self.day = day
+        self.weekday = weekday
+        self.month = month
+        self.year = year
+        self.tasks = []
+
+        self._today = date.today()
+        self._valid = bool(day)
+        if day == 0:
+            self._target = date.today()
+            self.day_name = u''
+        else:
+            self._target = date(year, month, day)
+            self.day_name = day_abbr[weekday]
+
+
+    def __repr__(self):
+        return u'<Day: %s>' % self._target.strftime('%d/%m/%Y')
+
+    @property
+    def today(self):
+        return self._today == self._target
+
+    @property
+    def past(self):
+        return self._today > self._target
+
+    @property
+    def future(self):
+        return self._today < self._target
+
+    @property
+    def valid(self):
+        return self._valid
+
+
+def process_week(week, month, year):
+    return [Day(*list(day) + [month, year]) for day in week]
+
+def check_tasks(day_list, tasks):
+    for day in day_list:
+        if day.valid and day.day in tasks:
+            day.tasks = tasks[day.day]
+    return day_list
+
 
 @register.inclusion_tag('schedule/task_calendar.html', takes_context=True)
 def calendar(context):
     month, year = context['month'], context['year']
     tasks = context['object_list']
     calendar = mark_safe(TaskCalendar(tasks).formatmonth(year, month))
+
+    dates = Calendar()
+    weeks = [process_week(week, month, year) for week in
+             dates.monthdays2calendar(year, month)]
+    weeks = [check_tasks(week, tasks) for week in weeks]
     return {
         'calendar': calendar,
+        'year': year,
+        'month': month,
+        'dates': dates.monthdays2calendar(year, month),
+        'weeks': weeks,
     }
