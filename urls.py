192ad951fbdc0e2b7df44dc7434f2ceda2c85e1a
Rob Berry <rob@luvhate.us>
2009-08-14 21:02:23 +0100

    include policy urls

--- urls.py
+++ urls.py
@@ -23,6 +23,7 @@ urlpatterns = patterns('',
     (r'^employees/', include('workers.urls')),
     (r'^skills/', include('workers.urls.skills')),
     (r'^schedules/', include('schedule.urls')),
+    (r'^projects/', include('policy.urls')),
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
 )
