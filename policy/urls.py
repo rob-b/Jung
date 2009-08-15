9f9bc8f1363be5416690d6cbab6bfc6d96a644df
Rob Berry <rob@luvhate.us>
2009-08-15 01:43:54 +0100

    project_detail url

--- policy/urls.py
+++ policy/urls.py
@@ -4,5 +4,9 @@ urlpatterns = patterns('policy.views',
     url(r'^$',
        'project_list',
        name='policy_project_list'),
+
+    url(r'^(?P<slug>[-\w]+)/$',
+        'project_detail',
+        name='policy_project_detail'),
 )
 
