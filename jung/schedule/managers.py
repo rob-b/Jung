3ced01b8300815386b43fb70107b7de72cb65388
Rob Berry <rob@luvhate.us>
2009-09-07 17:46:04 +0100

    manager method to find conflicted occurrences

--- jung/schedule/managers.py
+++ jung/schedule/managers.py
@@ -1,6 +1,7 @@
 from django.db import models
+from django.db import connection
 from datetime import date, timedelta, time, datetime
-from itertools import groupby
+from itertools import groupby, chain
 from collections import defaultdict
 from hostel.managers import QuerySet
 
@@ -8,12 +9,14 @@ from hostel.managers import QuerySet
 class OccurrenceManager(QuerySet):
 
     def for_user(self, user):
+        """Return all occurrences for `user`"""
         return self.filter(task__user=user)
 
     def month(self, month):
         return self.filter(start_time__month=month)
 
     def week_of(self, dt):
+        """All occurences that occur during the week that features `dt`"""
         try:
             dt = dt.date()
         except AttributeError:
@@ -34,6 +37,33 @@ class OccurrenceManager(QuerySet):
         # return a standard dict purely because there are places where django
         # doesn't handle defaultdict well i.e. templates
         return dict(days)
+
+    def conflicted(self):
+        """Return all occurrences that overlap with another occurrence.
+
+        bear in mind that most occurence will overlap and so this is only useful
+        when filtered with something else"""
+
+        sql = """SELECT DISTINCT  o1.id
+        FROM schedule_occurrence o1, schedule_occurrence o2
+        WHERE (o2.start_time BETWEEN o1.start_time AND o1.end_time
+        OR o2.end_time BETWEEN o1.start_time AND o1.end_time)
+        AND o1.id <> o2.id"""
+        cursor = connection.cursor()
+        cursor.execute(sql)
+        ids = chain(*cursor.fetchall())
+        return self.filter(id__in=ids).extra(select={'conflict': 1})
+
+    def pristine(self):
+        """Return all occurrences that do not overlap with any other"""
+        return self.exclude(
+            id__in=self.conflicted().values_list('id', flat=True)
+        )
+
+    def get_query_set(self):
+        qs = self.pristine() | self.conflicted()
+        return qs.order_by(self.model._meta.ordering)
+
 
 class TaskManager(QuerySet):
 
